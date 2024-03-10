0b31ec87912bf2208812e9effa4340f153a22cc0
Anshir08 <chaudharyanshir@gmail.com>
2024-03-10 17:25:37 +0530

    test-3

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -8,7 +8,7 @@ export const login = (email, password) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.post(
-      `https://mern-ecommerce-backend-bolo.onrender.com/api/v1/login`,
+      `api/v1/login`,
       { email, password },
       config
     );
@@ -26,7 +26,7 @@ export const register = (userData) => async (dispatch) => {
 
     const config = { headers: { "Content-Type": "multipart/form-data" } };
 
-    const { data } = await axios.post(`https://mern-ecommerce-backend-bolo.onrender.com/api/v1/register`, userData, config);
+    const { data } = await axios.post(`/api/v1/register`, userData, config);
 
     dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
   } catch (error) {
@@ -42,11 +42,10 @@ export const loadUser = () => async (dispatch) => {
   try {
     dispatch({ type: LOAD_USER_REQUEST });
 
-    const { data } = await axios.get(`https://mern-ecommerce-backend-bolo.onrender.com/api/v1/me`);
+    const { data } = await axios.get("https://mern-ecommerce-backend-bolo.onrender.com/api/v1/me");
 
     dispatch({ type: LOAD_USER_SUCCESS, payload: data.user });
   } catch (error) {
-    console.log(error);
     dispatch({ type: LOAD_USER_FAIL, payload: error.response.data.message });
   }
 }
@@ -54,7 +53,7 @@ export const loadUser = () => async (dispatch) => {
 // Logout User
 export const logout = () => async (dispatch) => {
   try {
-    await axios.get(`https://mern-ecommerce-backend-bolo.onrender.com/api/v1/logout`);
+    await axios.get(`/api/v1/logout`);
 
     dispatch({ type: LOGOUT_SUCCESS });
   } catch (error) {
@@ -68,7 +67,7 @@ export const updateProfile = (userData) => async (dispatch) => {
 
     const config = { headers: {"Content-Type": "multipart/form-data"}};
 
-    const {data} = await axios.put(`https://mern-ecommerce-backend-bolo.onrender.com/api/v1/me/update`, userData, config);
+    const {data} = await axios.put(`/api/v1/me/update`, userData, config);
 
     dispatch({type: UPDATE_PROFILE_SUCCESS, payload: data.success});
   } catch (error) {
@@ -87,7 +86,7 @@ export const updatePassword = (passwords) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" }};
 
     const { data } = await axios.put(
-      `https://mern-ecommerce-backend-bolo.onrender.com/api/v1/password/update`,
+      `/api/v1/password/update`,
       passwords,
       config
     );
@@ -108,7 +107,7 @@ export const forgotPassword = (email) => async (dispatch) => {
 
     const config = { headers: {"Content-Type": "application/json"}};
 
-    const { data } = await axios.post(`https://mern-ecommerce-backend-bolo.onrender.com/api/v1/password/forgot`, email, config);
+    const { data } = await axios.post(`/api/v1/password/forgot`, email, config);
 
     dispatch({ type: FORGOT_PASSWORD_SUCCESS, payload: data.message })
   } catch (error) {
@@ -127,7 +126,7 @@ export const resetPassword = (token, passwords) => async (dispatch) => {
     const config = { headers: {"Content-Type": "application/json"}};
 
     const { data } = await axios.put(
-      `https://mern-ecommerce-backend-bolo.onrender.com/api/v1/password/reset/${token}`,
+      `/api/v1/password/reset/${token}`,
       passwords, 
       config
     );
@@ -145,7 +144,7 @@ export const resetPassword = (token, passwords) => async (dispatch) => {
 export const getAllUsers = () => async (dispatch) => {
   try {
     dispatch({ type: ALL_USERS_REQUEST});
-    const { data } = await axios.get(`https://mern-ecommerce-backend-bolo.onrender.com/api/v1/admin/users`);
+    const { data } = await axios.get(`/api/v1/admin/users`);
 
     dispatch({type:ALL_USERS_SUCCESS, payload:data.users});
   } catch (error) {
@@ -173,7 +172,7 @@ export const updateUser = (id, userData) => async (dispatch) => {
     const config = { headers: {"Content-Type": "application/json"}};
 
     const { data } = await axios.put(
-      `https://mern-ecommerce-backend-bolo.onrender.com/api/v1/admin/user/${id}`,
+      `/api/v1/admin/user/${id}`,
       userData, 
       config
     );
@@ -189,7 +188,7 @@ export const deleteUser = (id) => async (dispatch) => {
   try {
     dispatch({type: DELETE_USER_REQUEST });
 
-    const { data } = await axios.delete(`https://mern-ecommerce-backend-bolo.onrender.com/api/v1/admin/user/${id}`);
+    const { data } = await axios.delete(`/api/v1/admin/user/${id}`);
 
     dispatch({ type: DELETE_USER_SUCCESS, payload: data });
   } catch(error) {
